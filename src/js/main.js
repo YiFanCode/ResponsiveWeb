5206a870d3a3f11b90c117fc6eca6210c59a8c64
YiFanMac <yifanmac@YiFanMacdeiMac.local>
2017-05-01 09:35:05 +0800

    responsiveWeb

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2017/4/5.
+ */
+$(document).ready(function(){
+    //owlCarousel ÂÖ²¥Í¼²å¼þ
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause:true
+    });
+});
